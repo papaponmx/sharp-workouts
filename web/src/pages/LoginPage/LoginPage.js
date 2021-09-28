7cea8f8de871a0fc3dfcc19cb491c353a6344a65
papaponmx <jaime.rios@hey.com>
2021-09-27 19:39:36 -0500

    Refactor login page to use LoginComponent

--- web/src/pages/LoginPage/LoginPage.js
+++ web/src/pages/LoginPage/LoginPage.js
@@ -1,11 +1,7 @@
-import { useAuth } from '@redwoodjs/auth'
 import { MetaTags } from '@redwoodjs/web'
-import { useState } from 'react'
+import { LoginForm } from '$src/components'
 
 const LoginPage = () => {
-  const [email, setEmail] = useState('')
-
-  const { logIn, logOut, signUp, isAuthenticated } = useAuth()
 
   return (
     <>
@@ -17,41 +13,7 @@ const LoginPage = () => {
       />
 
       <h1>LoginPage</h1>
-      <form action="#">
-        {!isAuthenticated && (
-          <input
-            type="email"
-            placeholder="email address"
-            required
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        )}
-        <button
-          disabled={!email.length && !isAuthenticated}
-          onClick={async () => {
-            if (!isAuthenticated && email.length) {
-              await logIn({ email, showUI: true })
-            } else {
-              await logOut()
-            }
-          }}
-        >
-          {isAuthenticated ? 'Log Out' : 'Log In'}
-        </button>
-
-        {!isAuthenticated && (
-          <button
-            disabled={!email.length && !isAuthenticated}
-            onClick={async () => {
-              if (!isAuthenticated && email.length) {
-                await signUp({ email, showUI: true })
-              }
-            }}
-          >
-            Sign Up
-          </button>
-        )}
-      </form>
+      <LoginForm />
     </>
   )
 }
