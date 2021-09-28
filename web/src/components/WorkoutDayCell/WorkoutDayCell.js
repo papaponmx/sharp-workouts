42ca2d7dac8d816c08802faf2309e184397720dc
papaponmx <jaime.rios@hey.com>
2021-09-27 19:39:51 -0500

    Generate workout-day cell.

---
+++ web/src/components/WorkoutDayCell/WorkoutDayCell.js
@@ -0,0 +1,19 @@
+export const QUERY = gql`
+  query FindWorkoutDayQuery($id: Int!) {
+    workoutDay: workoutDay(id: $id) {
+      id
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+)
+
+export const Success = ({ workoutDay }) => {
+  return <div>{JSON.stringify(workoutDay)}</div>
+}
