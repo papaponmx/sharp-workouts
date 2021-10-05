779fac66672bedc184b1b07dd7fb069ee67cce8d
papaponmx <jaime.rios@hey.com>
2021-10-04 21:44:36 -0500

    Removes requireAuth from roles page.

--- api/src/services/roles/roles.ts
+++ api/src/services/roles/roles.ts
@@ -1,10 +1,8 @@
 import type { Prisma } from '@prisma/client'
 import type { ResolverArgs } from '@redwoodjs/graphql-server'
-import { requireAuth } from 'src/lib/auth'
 import { db } from 'src/lib/db'
 
 export const roles = () => {
-  requireAuth()
   return db.role.findMany()
 }
 
